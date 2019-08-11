a3eb218c08d7f5ab0a805fdcfb8e7ba4f336bfc5
svipas <benas.svipas@gmail.com>
2019-08-11 20:46:43 +0300

    Bundle extension with webpack

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+
+/**@type {import('webpack').Configuration}*/
+const config = {
+  target: 'node',
+  entry: './src/extension.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'extension.js',
+    libraryTarget: 'commonjs2',
+    devtoolModuleFilenameTemplate: '../[resource-path]'
+  },
+  devtool: 'source-map',
+  externals: {
+    vscode: 'commonjs vscode',
+    prettier: 'commonjs prettier',
+    'prettier-eslint': 'commonjs prettier-eslint',
+    'prettier-tslint': 'commonjs prettier-tslint',
+    'prettier-stylelint': 'commonjs prettier-stylelint'
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.json']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'ts-loader'
+          }
+        ]
+      }
+    ]
+  }
+};
+
+module.exports = config;
